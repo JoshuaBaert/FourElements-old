4d5d349d0af545c3fd6e82c98d0be58ff2a2a77f
Joshua Baert <jbghoring@gmail.com>
2017-06-29 12:20:30 -0500

    Nav setup

--- public/src/app/app.component.ts
+++ public/src/app/app.component.ts
@@ -2,8 +2,14 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
+  template: `    
+
+    <app-nav></app-nav>
+
+    <router-outlet></router-outlet>
+    
+  `
 })
 export class AppComponent {
   title = 'app works!';
