6e6bec2d5559cc3d749b0eb5c43125242b980025
Joshua Baert <jbghoring@gmail.com>
2017-06-29 11:23:14 -0500

    Nav setup

--- public/src/app/nav/nav.component.ts
+++ public/src/app/nav/nav.component.ts
@@ -2,8 +2,17 @@ import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-nav',
-  templateUrl: './nav.component.html',
-  styleUrls: ['./nav.component.scss']
+  styleUrls: ['./nav.component.scss'],
+  template: `
+    <div class="nav-div">
+      <div class="nav-container">
+        <img src="http://lorempixel.com/50/50/abstract" alt="Logo">
+        <a routerLink=""><span>HOME</span></a>
+        <a routerLink="work"><span>WORK</span></a>
+      </div>
+    </div>
+    
+  `
 })
 export class NavComponent implements OnInit {
 
